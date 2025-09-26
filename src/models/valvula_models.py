e2683aec10f0e486586562792c905c51608b36fa
filipeavila7 <filipeavila076@gmail.com>
2025-09-26 09:21:04 -0300

    rota de cadastro e login

--- src/models/valvula_models.py
+++ src/models/valvula_models.py
@@ -7,3 +7,10 @@ class Valvulas(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  
     nome_valvula = db.Column(db.String(120), nullable=False)
     local_valvula = db.Column(db.String(120), nullable=True)
+
+
+    def __init__(self, nome, email, data_login, senha):
+        self.__nome = nome #esat vindo da view
+        self.__email = email
+        self.__data_login = data_login
+        self.__senha = senha
